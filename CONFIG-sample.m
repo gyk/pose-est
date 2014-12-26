e903e4bf5f6f67a3357cdf9c8494144d555fd014
Yukun Guo <gyk.net@gmail.com>
2014-12-26 11:22:00 +0800

    Update for synthetic data

--- CONFIG-sample.m
+++ CONFIG-sample.m
@@ -1,11 +1,16 @@
 classdef CONFIG
 	properties (Constant)
 
+	HE_PATH = 'F:\HumanEva\';
+
 	% saved video frames
 	SNAPSHOT_PATH = 'F:\silhouette\video\';
 
 	% generated silhouettes
 	SILHOUETTE_PATH = 'F:\silhouette\out_action\Humanoid';
+
+	SYNTH_TR_SUFFIX = 'S_TRAIN';
 	
 	end
+
 end
